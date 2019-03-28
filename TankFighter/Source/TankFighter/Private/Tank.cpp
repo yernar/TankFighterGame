215a8a1c39294769f485b8f8c3ca5fe205f5fbb1
Yernar Aldabergenov <yernar.aa@gmail.com>
2019-03-28 19:40:00 +0600

    TankBarrel Static Mesh resetting bug fixed

--- TankFighter/Source/TankFighter/Private/Tank.cpp
+++ TankFighter/Source/TankFighter/Private/Tank.cpp
@@ -7,7 +7,7 @@
 ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	TankAimComponent = CreateDefaultSubobject<UTankAimComponent>(FName("Aim Components"));
 }
