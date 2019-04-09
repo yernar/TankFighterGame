06916bef013468afb0ef451461bceb13f51e8c6c
Yernar Aldabergenov <yernar.aa@gmail.com>
2019-04-10 03:18:40 +0600

    Improved physics

--- TankFighter/Source/TankFighter/Private/TankTrackStaticMeshComponent.cpp
+++ TankFighter/Source/TankFighter/Private/TankTrackStaticMeshComponent.cpp
@@ -5,17 +5,12 @@
 
 UTankTrackStaticMeshComponent::UTankTrackStaticMeshComponent()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTankTrackStaticMeshComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	UStaticMeshComponent* TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
-	FVector CorrectionAcceleration = (-FVector::DotProduct(GetRightVector(), GetComponentVelocity())) / DeltaTime * GetRightVector(); // Accelartion
-	FVector CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Divide to 2, as we have 2 Tracks
-	TankRoot->AddForce(CorrectionForce);
 }
 
 void UTankTrackStaticMeshComponent::BeginPlay()
@@ -25,10 +20,26 @@ void UTankTrackStaticMeshComponent::BeginPlay()
 
 void UTankTrackStaticMeshComponent::OnCompHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	UE_LOG(LogTemp, Warning, TEXT("HIITTTTING"))
+	DriveTracks();
+	SidewaysForce();
+	Throttle = 0.f;
 }
 
-void UTankTrackStaticMeshComponent::SetThrottle(float Throttle)
+void UTankTrackStaticMeshComponent::SidewaysForce()
+{
+	UStaticMeshComponent* TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	FVector CorrectionAcceleration = (-FVector::DotProduct(GetRightVector(), GetComponentVelocity())) / (GetWorld()->GetDeltaSeconds()) * GetRightVector(); // Accelartion
+	FVector CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Divide to 2, as we have 2 Tracks
+	TankRoot->AddForce(CorrectionForce);
+}
+
+void UTankTrackStaticMeshComponent::SetThrottle(float CurrentThrottle)
+{
+	Throttle = FMath::Clamp<float>(Throttle + CurrentThrottle, -1.f, 1.f);
+	// DriveTracks(Throttle);
+}
+
+void UTankTrackStaticMeshComponent::DriveTracks()
 {
 	FVector ForceApplied = GetForwardVector() * MaxTrackDrivingF * Throttle;
 	FVector ForceLocation = GetComponentLocation();
